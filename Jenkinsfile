dbab6c394ae37f48b57c04777dff79bfa7d0120b
Govardhangir <govardhangiri.m@gmail.com>
2026-01-09 07:12:51 +0530

    jenkins

---
+++ Jenkinsfile
@@ -0,0 +1,9 @@
+stage('Debug PR Context') {
+    steps {
+        sh '''
+          echo "CHANGE_ID=$CHANGE_ID"
+          echo "CHANGE_BRANCH=$CHANGE_BRANCH"
+          echo "CHANGE_TARGET=$CHANGE_TARGET"
+        '''
+    }
+}
--- backend/app/alembic/versions/1a31ce608336_add_cascade_delete_relationships.py
+++ backend/app/alembic/versions/1a31ce608336_add_cascade_delete_relationships.py
@@ -8,7 +8,7 @@ Create Date: 2024-07-31 22:24:34.447891
 from alembic import op
 import sqlalchemy as sa
 import sqlmodel.sql.sqltypes
-gg
+
 # revision identifiers, used by Alembic.
 revision = '1a31ce608336'
 down_revision = 'd98dd8ec85a3'
